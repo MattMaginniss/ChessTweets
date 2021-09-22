5088e5ef71ae861b1765d2074ef7370d0b84fea7
Matthew Maginniss <Matthew.Maginniss@gmail.com>
2021-09-22 18:34:21 -0400

    Generated image from PGN

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var ChessImageGenerator = require('chess-image-generator')
+
+var imageGenerator = new ChessImageGenerator({
+  size: 1200,
+  light: '#c7c7c7',
+  dark: '#71828f',
+  style: 'cburnett',
+  flipped: false
+})
+
+let pgn = '1. Nf3 d5 2. e3 Nf6 3. d4 c5 4. h3 Nc6 5. c4 cxd4 6. exd4 e6 7. Nc3 Be7'
+imageGenerator.loadPGN(pgn)
+imageGenerator.generatePNG('./output.png')
+
